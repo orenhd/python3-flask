796ca4221e22126e827a646275fad93dfba624f7
Oren Hadar <orenhd123@gmail.com>
2019-02-20 22:47:43 +0200

    separate controllers from routes

---
+++ controllers/user_controllers.py
@@ -0,0 +1,73 @@
+import json
+from bcrypt import hashpw, gensalt, checkpw
+from jwt import encode as jwt_encode, decode as jwt_decode
+
+from models.user_model import UserModel
+
+import config
+
+import consts.user_consts as user_consts
+
+SALT_WORK_FACTOR = 10
+
+
+def user_signup_controller(req, res, abort):
+    if not req.json or not 'username' in req.json:
+        abort(400)
+    UserModel(
+        username=req.json['username'],
+        password=hashpw(req.json['password'].encode(
+            'UTF-8'), gensalt(SALT_WORK_FACTOR))
+    ).save()
+    resp = res(json.dumps({'success': True}), 200)
+    resp.headers['content-type'] = 'application/json'
+    return resp
+
+
+def user_login_controller(req, res, abort):
+    if not req.json or not 'username' in req.json:
+        abort(400)
+    requested_user = UserModel.objects(  # pylint: disable=no-member
+        username=req.json['username']).first()
+    if not requested_user:
+        resp = res(json.dumps(
+            {'success': True, 'msg': user_consts.AUTH_FAILED_USER_MSG}), 401)
+    if checkpw(req.json['password'].encode(
+            'UTF-8'),
+            requested_user['password'].encode(
+            'UTF-8')):
+
+        # Create access token based on the logged in username
+        # Use jwt directly to maintain controller independent from app instance
+        access_token = jwt_encode(
+            {'identity': requested_user['username']}, config.app['at_string'], algorithm='HS256')
+        resp = res(json.dumps({'success': True, 'data': {'access_token': access_token.decode(
+            "utf-8")}, 'msg': user_consts.AUTH_SUCCESS_MSG}), 200)
+    else:
+        resp = res(json.dumps(
+            {'success': True, 'msg': user_consts.AUTH_FAILED_PASSWORD_MSG}), 401)
+    resp.headers['content-type'] = 'application/json'
+    return resp
+
+
+def user_get_controller(req, res, abort, **kwargs):
+    requested_user = UserModel.objects(  # pylint: disable=no-member
+        username=kwargs.get('username')).exclude('password').first()
+    # Use jwt directly to maintain controller independent from app instance
+    jwt_decoded = jwt_decode(req.headers.get(
+        'x-access-token'), config.app['at_string'], algorithms=['HS256'])
+    logged_in_user = UserModel.objects(  # pylint: disable=no-member
+        username=jwt_decoded['identity']).exclude('password').first()
+    if requested_user and logged_in_user:
+        # Verify friendship
+        requested_user_id_str = str(requested_user.id)
+        is_friend = True if requested_user_id_str in logged_in_user['friends'] else False
+        resp = res(json.dumps({'success': True, 'data': {'id': requested_user_id_str,
+                                                         'username': requested_user['username'],
+                                                         'friends': requested_user['friends'],
+                                                         'is_friend': is_friend}}),
+                   200)
+        resp.headers['content-type'] = 'application/json'
+        return resp
+    else:
+        abort(400)
